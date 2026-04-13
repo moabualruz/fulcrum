51a1742f96b3ef3de1b2e2daffb4b9ff9931b796
Mo Abualruz <mkh.ruzz@live.com>
2026-04-13 23:46:17 +0200

    chore(workflows): scaffold @fulcrum/workflows package

---
+++ packages/workflows/src/index.ts
@@ -0,0 +1,6 @@
+// packages/workflows/src/index.ts
+export * from './types.js'
+export * from './schema.js'
+export * from './registry.js'
+export * from './engine.js'
+export * from './workflows.js'
